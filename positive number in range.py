0e2ef9275c8b558e8431c3b3358250eb7357abc9
Shashikant Shrivastava <89238030+shashikant13@users.noreply.github.com>
2021-10-18 15:48:53 +0530

    positive number in range

---
+++ positive number in range.py
@@ -0,0 +1,11 @@
+# Python program to print positive Numbers in a List
+  
+# list of numbers
+list1 = [12, -7, 5, 64, -14]
+  
+# iterating each number in list
+for num in list1:
+      
+    # checking condition
+    if num >= 0:
+       print(num, end = " ")
